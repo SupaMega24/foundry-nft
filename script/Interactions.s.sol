93db380de154d96a2bff85d0e52c4cb117114ffe
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-05-15 22:57:33 +0900

    Refactor

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,7 +7,7 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
     string public constant PUP =
-        "ipfs://bafybeicz57su3zyafexgchnfqej7zuxfa3e4xgz22yzmge6nzkhfmskqs4/";
+        "ipfs://bafybeicz57su3zyafexgchnfqej7zuxfa3e4xgz22yzmge6nzkhfmskqs4/?filename=QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
